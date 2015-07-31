dea9ed83494c124f8f036ca2dde51a85761da340
Warshavski <shikari005@gmail.com>
2015-07-31 13:07:12 +0400

    Update enter point of program (set FormMain as start form)

--- Importer/Importer.UI.WinView/Program.cs
+++ Importer/Importer.UI.WinView/Program.cs
@@ -15,7 +15,7 @@ namespace Importer.UI.WinView
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Importer.UI.WinView.Forms.FormMain());
         }
     }
 }
