e77f34d20ff209de8b610af2f56f0f853203cf7c
Warshavski <shikari005@gmail.com>
2015-07-31 16:27:44 +0400

    Update Import method and class constructor

--- Importer/Importer.Engine/Models/Importers/SqlCopy.cs
+++ Importer/Importer.Engine/Models/Importers/SqlCopy.cs
@@ -4,102 +4,153 @@ using System.Data.SqlClient;
 namespace Importer.Engine.Models.Importers
 {
     /* TODO : 
-     *   1. Add constructors
-     *   2. Add a function to check the allowable values
-     *   3. Add type conversion function
-     *   4. Add import method for tables with fewer rows
-     *   5. Rewrite SqlBulk import method
-     */
+      *   1. - done (+/-) - Add constructors (rewrite)
+      *   2. Add a function to check the allowable values
+      *   3. Add type conversion function
+      *   4. Add import method for tables with fewer rows
+      *   5. - done - Rewrite SqlBulk import method
+      *   6. Add something for operation status indications
+     *    7. ?? Add SqlBulkCopyOptions in SqlBulkCopy constructor ??
+     *    8. ?? Errors and copy abort ??
+      */
 
     public class SqlCopy : ICopy
     {
+        #region Class constants
+
         private const int BATCH_SIZE = 1000;
         private const int NOTIFY_AFTER = 1;
-        //private const int TIMEOUT = 0; 
 
-        private SqlBulkCopy _sqlCopy = null;
+        #endregion
+
+        #region Class delegates
+
         private CopyProgress _ChangeInStatus;
 
-        private int _rowsCount = 0;
+        #endregion
+
+        private int _rowsTotal = -1;
+
+        #region Constructors
 
         public SqlCopy(CopyProgress ChangeInStatus)
         {
             _ChangeInStatus = ChangeInStatus;
-
-            // initialization of _sqlCopy 
-            _sqlCopy.NotifyAfter = NOTIFY_AFTER;
-            _sqlCopy.BatchSize = BATCH_SIZE;
-            _sqlCopy.SqlRowsCopied += new SqlRowsCopiedEventHandler(OnSqlRowsCopied);
         }
 
-        private void CreateSqlBulkMapping(ColumnsMapping colMapp)
-        {
-            _sqlCopy.ColumnMappings.Add(
-                new SqlBulkCopyColumnMapping(colMapp.SourceColumn, colMapp.TargetColumn));
-        }
+        #endregion
 
-        
         private void OnSqlRowsCopied(object sender, SqlRowsCopiedEventArgs e)
         {
-            // (int)((100 * e.RowsCopied) / _testRowsCount)
-            _ChangeInStatus((int)((100 * e.RowsCopied) / _rowsCount));
+            // (int)((100 * e.RowsCopied) / [total rows])
+            _ChangeInStatus((int)((100 * e.RowsCopied) / _rowsTotal));
         }
+
+        #region Public members
 
         public bool Import(Table sourceTable, Table targetTable, ColumnsMapping[] columnsMapping, bool truncate)
         {
-            _rowsCount = sourceTable.RowsCount;
+            _rowsTotal = sourceTable.RowsCount;
+            string commandText = string.Empty;
             
-            /*
-             * string commandText = string.Format("SELECT {0} FROM [{1}]",
-             * string.Join(", ", sourceTable.Columns.Select(i => i.Name).ToArray<string>()),
-             *     sourceTable.Name);
-             */
-            
-            _sqlCopy = new SqlBulkCopy(targetTable.ConnectionString);
-
-            _sqlCopy.DestinationTableName = targetTable.Name;
-
-           
-
-            try
+            using (DbConnection sourceConnection = CommonData.CreateDbConnection(sourceTable.ProviderName, sourceTable.ConnectionString))
             {
-                string commandText = string.Empty;
+                commandText = string.Format("SELECT * FROM [{0}]", sourceTable.Name);
 
-                using (DbConnection targetConnection = CommonData.CreateDbConnection(
-                    targetTable.ProviderName, targetTable.ConnectionString))
+                sourceConnection.Open();
+                using (DbDataReader reader = CommonData.CreateCommand(commandText, sourceConnection).ExecuteReader())
                 {
-                    targetConnection.Open();
-
-                    if (truncate)
+                    using (DbConnection targetConnection = CommonData.CreateDbConnection(targetTable.ProviderName, targetTable.ConnectionString))
                     {
-                        commandText = string.Format("TRUNCATE TABLE [{0}]", targetTable.Name);
-                        CommonData.CreateCommand(commandText, targetConnection).ExecuteNonQuery();
-                    }
+                        targetConnection.Open();
 
-                    using (DbConnection sourceConnection = CommonData.CreateDbConnection(
-                        sourceTable.ProviderName, sourceTable.ConnectionString))
-                    {
-                        foreach (ColumnsMapping element in columnsMapping)
-                            CreateSqlBulkMapping(element);
-                        
-                        sourceConnection.Open();
+                        if (truncate)
+                        {
+                            commandText = string.Format("TRUNCATE TABLE dbo.[{0}]", targetTable.Name);
+                            CommonData.CreateCommand(commandText, targetConnection).ExecuteNonQuery();
+                        }
 
-                        commandText = string.Format("SELECT * FROM [{0}]", sourceTable.Name);
-                        
-                        using (DbDataReader reader = CommonData.CreateCommand(
-                            commandText, sourceConnection).ExecuteReader())
-                            _sqlCopy.WriteToServer(reader);
-                        sourceConnection.Close();
+                        using (SqlBulkCopy bulkCopy = new SqlBulkCopy(targetConnection.ConnectionString))
+                        {
+                            bulkCopy.BatchSize = BATCH_SIZE;
+                            bulkCopy.NotifyAfter = NOTIFY_AFTER;
+                            bulkCopy.SqlRowsCopied += new SqlRowsCopiedEventHandler(OnSqlRowsCopied);
+
+                            // TODO : m.b. create separate function
+                            foreach (ColumnsMapping element in columnsMapping)
+                                bulkCopy.ColumnMappings.Add(
+                                    new SqlBulkCopyColumnMapping(element.SourceColumn, element.TargetColumn));
+                            bulkCopy.WriteToServer(reader);
+                        }
+                        targetConnection.Close();
                     }
-                    targetConnection.Close();
                 }
-                return true;
+                sourceConnection.Close();
             }
-            finally
-            {
-                _sqlCopy.Close();
-                _sqlCopy = null;
-            }
+            return true;
         }
+
+        #endregion
     }
 }
+
+
+
+/*
+* string commandText = string.Format("SELECT {0} FROM [{1}]",
+* string.Join(", ", sourceTable.Columns.Select(i => i.Name).ToArray<string>()),
+*     sourceTable.Name);
+*/
+
+/* Old import variant
+public bool Import(Table sourceTable, Table targetTable, ColumnsMapping[] columnsMapping, bool truncate)
+{
+    _rowsTotal = sourceTable.RowsCount;
+    
+    _sqlCopy = new SqlBulkCopy(targetTable.ConnectionString);
+
+    _sqlCopy.DestinationTableName = targetTable.Name;
+
+    try
+    {
+        string commandText = string.Empty;
+
+        using (DbConnection targetConnection = CommonData.CreateDbConnection(
+            targetTable.ProviderName, targetTable.ConnectionString))
+        {
+            targetConnection.Open();
+
+            if (truncate)
+            {
+                commandText = string.Format("TRUNCATE TABLE [{0}]", targetTable.Name);
+                CommonData.CreateCommand(commandText, targetConnection).ExecuteNonQuery();
+            }
+
+            using (DbConnection sourceConnection = CommonData.CreateDbConnection(
+                sourceTable.ProviderName, sourceTable.ConnectionString))
+            {
+                // TODO : m.b. create separate function
+                foreach (ColumnsMapping element in columnsMapping)
+                    _sqlCopy.ColumnMappings.Add(
+                        new SqlBulkCopyColumnMapping(element.SourceColumn, element.TargetColumn));
+                
+                sourceConnection.Open();
+
+                commandText = string.Format("SELECT * FROM [{0}]", sourceTable.Name);
+                
+                using (DbDataReader reader = CommonData.CreateCommand(
+                    commandText, sourceConnection).ExecuteReader())
+                    _sqlCopy.WriteToServer(reader);
+                sourceConnection.Close();
+            }
+            targetConnection.Close();
+        }
+        return true;
+    }
+    finally
+    {
+        _sqlCopy.Close();
+        _sqlCopy = null;
+    }
+}
+*/
