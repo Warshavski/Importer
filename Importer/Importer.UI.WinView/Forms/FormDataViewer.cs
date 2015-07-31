5e903136a1305299d548ea3dd2d1e106043af899
Warshavski <shikari005@gmail.com>
2015-07-31 14:42:29 +0400

    Add data viewer form to UI (Windows form project)

---
+++ Importer/Importer.UI.WinView/Forms/FormDataViewer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Importer.UI.WinView.Forms
+{
+    public partial class FormDataViewer : Form
+    {
+        public FormDataViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void EventHandler_FormDataViewer_SizeChanged(object sender, EventArgs e)
+        {
+            const int DIVIDER = 2;
+            progressBar1.Location =
+                new System.Drawing.Point(progressBar1.Location.X, this.dataGridView1.Height / DIVIDER - progressBar1.Height / DIVIDER);
+        }
+    }
+}
