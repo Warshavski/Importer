4f7060b708aaff223d7d27e127fcaea28f5c9ab9
Warshavski <shikari005@gmail.com>
2015-08-04 17:33:13 +0400

    Add table create form to UI (Winows forms project)

---
+++ Importer/Importer.UI.WinView/Forms/FormCreateTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Importer.UI.WinView.Forms
+{
+    public partial class FormCreateTable : Form
+    {
+        public FormCreateTable()
+        {
+            InitializeComponent();
+        }
+    }
+}
